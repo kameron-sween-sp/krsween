fffbbcb9e69b0cbd5d5b7cc70cb24f3f7f7fdff1
Kameron R. Sween <kameron@krsween.com>
2017-01-06 15:34:45 -0600

    Initial

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { KrsweenPage } from './app.po';
+
+describe('krsween App', function() {
+  let page: KrsweenPage;
+
+  beforeEach(() => {
+    page = new KrsweenPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
